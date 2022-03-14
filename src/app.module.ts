2575fc7413e6398cc8d7adade64beebe22d4604a
Tiago Antunes <tiago_antunes14@gmail.com>
2022-03-14 14:19:44 -0400

    Structure of transaction operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AccountModule } from './account/account.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [EventEmitterModule.forRoot(), AccountModule],
+  imports: [EventEmitterModule.forRoot(), AccountModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
