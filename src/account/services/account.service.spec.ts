3ecaf41323f96ba55a81f39b39e4149c8c2257ef
Tiago Antunes <tiago_antunes14@gmail.com>
2022-03-23 09:31:45 -0400

    Fix tests

--- src/account/services/account.service.spec.ts
+++ src/account/services/account.service.spec.ts
@@ -1,47 +1,48 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AccountService, AccountServiceImpl } from "./account.service";
-//
-// import { HttpStatus } from "@nestjs/common";
-// import { Account } from "../entities/account.entity";
-//
-// describe('AccountService', () => {
-//   let service: AccountServiceImpl;
-//
-//   const fakeAccount = new Account();
-//   fakeAccount.name = 'Thomas';
-//   fakeAccount.document = '000.000.000-11';
-//   fakeAccount.availableLimit = 0;
-//
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AccountServiceImpl],
-//     })
-//       .useMocker((token) => {
-//         if (token === 'AccountRepository') {
-//           return {
-//             findAccountByDocument: jest.fn().mockResolvedValue(fakeAccount),
-//             create: jest.fn().mockResolvedValue(fakeAccount),
-//           };
-//         }
-//       })
-//       .compile();
-//
-//     service = module.get<AccountServiceImpl>(AccountServiceImpl);
-//   });
-//
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-//
-//   it('should return a conflict error', async () => {
-//     await service.createAccount({
-//       name: 'Teste 1',
-//       document: '000.000.000-00',
-//       availableLimit: 1,
-//     })
-//       .catch(reject => {
-//       expect(reject['status']).toBe(HttpStatus.CONFLICT);
-//     });
-//   });
-//
-// });
+import { Test, TestingModule } from '@nestjs/testing';
+import { AccountService, AccountServiceImpl } from "./account.service";
+
+import { HttpStatus } from "@nestjs/common";
+import { Account } from "../entities/account.entity";
+import AccountBuilder from "../entities/account.builder";
+
+describe('AccountService', () => {
+  let service: AccountServiceImpl;
+
+  const fakeAccount = new AccountBuilder('000.000.000-01')
+    .setName('Ronald')
+    .setAvailableLimit(100)
+    .build();
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AccountServiceImpl],
+    })
+      .useMocker((token) => {
+        if (token === 'AccountDAO') {
+          return {
+            findAccountByDocument: jest.fn().mockResolvedValue(fakeAccount),
+            create: jest.fn().mockResolvedValue(fakeAccount),
+          };
+        }
+      })
+      .compile();
+
+    service = module.get<AccountServiceImpl>(AccountServiceImpl);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return a conflict error', async () => {
+    await service.createAccount({
+      name: 'Teste 1',
+      document: '000.000.000-00',
+      availableLimit: 1,
+    })
+      .catch(reject => {
+      expect(reject['status']).toBe(HttpStatus.CONFLICT);
+    });
+  });
+
+});
